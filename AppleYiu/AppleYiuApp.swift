413df7c92a2045c896694231e43986318a06095d
Your-baiqiang <Your-baiqiang@users.noreply.github.com>
2023-09-11 13:20:09 +0800

    Initial Commit

---
+++ AppleYiu/AppleYiuApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleYiuApp.swift
+//  AppleYiu
+//
+//  Created by MacBook on 2023/9/11.
+//
+
+import SwiftUI
+
+@main
+struct AppleYiuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
